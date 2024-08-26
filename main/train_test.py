1c1b01ac6341cc84ffe10b5bb303395dda53da64
jackyang <48745667+jackyang-star@users.noreply.github.com>
2024-08-26 16:00:30 +0800

    完成MVCG模型; 完成train_test.py

--- main/train_test.py
+++ main/train_test.py
@@ -1,24 +1,42 @@
+import logging
+import os
+import time
+
 import numpy as np
 import pandas as pd
+import random
 import dgl
+from tqdm import tqdm
 import torch
 from torch import nn
 import scipy.sparse as sp
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader, random_split
-from model.model import EarlyStopping
-from model.model2 import GraphSAGE, Predictor, MVCG
+from torch.utils.data import DataLoader
+from model.model2 import MVCG, EarlyStopping, MyDataset
 from utils.create_cm_fv_utils import create_fv_list, create_api_cooccurrence_matrix, create_fv_cooccurrence_matrix
 
 
-# 参数
+# parameters
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# data parameters
 embedding_dim = 64
-h_dim = 16
-num_epochs = 1000
-patience = 25
-delta = 0
-ifverbose = False
+train_test_ratio = 0.8
+# train & test parameters
+learning_rate = 0.001
+num_epochs = 130
+topk = 10
+# model parameters
+dropout_rate = 0.1
+combine_layer_num = 1
+# early_stopping parameters
+es_patience = 20
+es_delta = 0
+es_verbose = False
+# scheduler parameters
+s_patience = 10
+s_verbose = True
+s_factor = 0.1
+s_mode = 'min'
 
 
 # 构建API的共现矩阵
@@ -64,31 +82,168 @@ def create_dgl_weighted_graph(adj_matrix):
     adj_matrix = adj_matrix.astype(np.int32)
     sp_matrix = sp.coo_matrix(adj_matrix)
     g = dgl.from_scipy(sp_matrix, eweight_name='weight')
-    g.edata['weight'] = g.edata['weight'].type(torch.int32)
-    return g
+    g.edata['weight'] = g.edata['weight'].type(torch.float32)
+    return g.to(device)
 
 def create_graphs(features, api_df, api_list_array, api_cm_array):
-    # max_length = 0
-    # adj_matrices = []
-    # graph_names = []
+    max_feature_length = 0
     graphs = []
     for feature in features:
         fv_list_array = create_fv_list(api_df, api_list_array, feature)  # 获取feature_value列表
         cm_array, max_fv_len = create_fv_cooccurrence_matrix(api_cm_array, api_list_array, api_df, fv_list_array,
                                                              feature)  # 构建feature_value共现矩阵
         graphs.append(create_dgl_weighted_graph(cm_array))
-        # adj_matrices.append(cm_array)
-        # graph_names.append(feature)
-        # if max_fv_len > max_length:
-        #     max_length = max_fv_len  # 获取特征值最大数量
-    # graphs = [create_dgl_weighted_graph(adj) for adj in adj_matrices]
-    # np.savetxt(f'../dataset/processed/cm_fv/max_fv_length.txt', np.array([max_length]), fmt='%d')  # 保存特征值最大数量
-    return graphs
+        if max_fv_len > max_feature_length:
+            max_feature_length = max_fv_len  # 获取特征值最大数量
+    return graphs, max_feature_length
 
 
-def prepare_data(mvcg, graphs, node_embeddings):
+# train_data(query, pos, label; query, neg, label); test_data(query, candidates, truths)
+def prepare_data(api_cm_array, api_list_array, api_df, features):
+    train_data = []
+    test_data = []
+    strategies = []
 
-    return train_pairs, labels
+    fv_dict = {}
+    for feature in features:
+        fv_dict[feature] = create_fv_list(api_df, api_list_array, feature)
+
+    for i in range(api_cm_array.shape[0]):
+        non_zero_columns = np.nonzero(api_cm_array[i])[0].tolist()
+        zero_columns = np.where(api_cm_array[i] == 0)[0].tolist()
+        train_length = int(len(non_zero_columns) * train_test_ratio)
+        # 构建训练数据
+        positives = non_zero_columns[0:train_length]
+        negatives = [col for col in zero_columns if col != i]  # 负样本要排除自身
+        negatives = random.sample(negatives, min(train_length, len(negatives)))
+        for pos in positives:
+            train_data.append((i, pos, 1))
+        for neg in negatives:
+            train_data.append((i, neg, 0))
+        # 构建测试数据
+        truths = non_zero_columns[train_length:]
+        all = list(range(api_cm_array.shape[1]))
+        candidates = list(set(all) - set(positives) - {i})
+        test_data.append(([i], candidates, truths))
+
+        api_name = api_list_array[i]  # 根据行号，从api_list中找到api的名称
+        matching_api = api_df[api_df['Name'] == api_name]  # 根据api_name，从api_df中找到对应的行
+        item = {}
+        for index, feature in enumerate(features):
+            matching_feature = matching_api[feature].iloc[0]  # 获取特征值
+            if not pd.isna(matching_feature):
+                indexes = []
+                matching_feature = [e.strip() for e in matching_feature.split(',')]  # 特征值保存为列表形式
+                for fv in matching_feature:
+                    fv_index = np.where(fv_dict[feature] == fv)[0][0]  # 通过特征值的名称在特征值列表中找到特征值的编号
+                    indexes.append(fv_index)
+                item[index] = indexes
+        strategies.append(item)
+
+    return train_data, test_data, strategies
+
+
+def train(dataloader, model, optimizer, loss_fn, max_feature_length, graphs):
+    model.train()
+    epoch_loss = []
+    for query, candidate, label in dataloader:
+        # cuda上运行
+        query = query.to(device)
+        candidate = candidate.to(device)
+        label = label.to(torch.float32).to(device)
+        # 计算complementary score
+        complementary_score = model(graphs, query, candidate, max_feature_length)
+        # 计算损失
+        loss = loss_fn(complementary_score, label)
+        # 反向传播和优化
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # 记录loss
+        epoch_loss.append(loss.item())
+    return epoch_loss
+
+
+def recall(pred, truth):
+    hit_num = len(set(pred) & set(truth))
+    recall = hit_num / len(truth)
+    return recall
+
+
+def mrr(pred, truth):
+    reciprocal_rank = 0
+    for rank, item in enumerate(pred, start=1):
+        if item in truth:
+            reciprocal_rank = 1 / rank
+            break
+    return reciprocal_rank
+
+
+def dcg(r):
+    r = np.asfarray(r)
+    if r.size:
+        return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+    return 0.0
+def ndcg(pred, truth):
+    ideal = [1] * len(set(truth) & set(pred))
+    actual = [1 if item in truth else 0 for item in pred]
+    idcg = dcg(ideal)
+    adcg = dcg(actual)
+    ndcg = adcg / idcg if idcg > 0 else 0
+    return ndcg
+
+
+def test(dataloader, model, graphs, feature_length, topk):
+    model.eval()
+    with torch.no_grad():
+        recall_value = []
+        mrr_value = []
+        ndcg_value = []
+        for query, candidates, truths in tqdm(dataloader):
+            query = torch.tensor(query, dtype=torch.int32).to(device)
+            truths = torch.tensor(truths, dtype=torch.int32).to(device)
+            candidates = torch.tensor(candidates, dtype=torch.int32).to(device)
+            # 计算得分
+            scores = []  # 保存candidate编号和score
+            for candidate in candidates:
+                score = model(graphs, query, candidate, feature_length)
+                scores.append((candidate, score))
+            scores.sort(key=lambda x: x[1], reverse=True)
+            topk_candidates = [x[0].item() for x in scores[:topk]]
+            # 计算指标
+            truths = truths.tolist()
+            recall_value.append(recall(topk_candidates, truths))
+            mrr_value.append(mrr(topk_candidates, truths))
+            ndcg_value.append(ndcg(topk_candidates, truths))
+        avg_recall = sum(recall_value) / len(recall_value)
+        avg_mrr = sum(mrr_value) / len(mrr_value)
+        avg_ndcg = sum(ndcg_value) / len(ndcg_value)
+    return avg_recall, avg_mrr, avg_ndcg
+
+
+def create_logger(logger_file_path):
+    if not os.path.exists(logger_file_path):
+        os.makedirs(logger_file_path)
+    log_name = '{}.log'.format(time.strftime('%Y-%m-%d-%H-%M'))
+    final_log_file = os.path.join(logger_file_path, log_name)
+
+    logger = logging.getLogger()  # 设定日志对象
+    logger.setLevel(logging.INFO)  # 设定日志等级
+
+    file_handler = logging.FileHandler(final_log_file)  # 文件输出
+    console_handler = logging.StreamHandler()  # 控制台输出
+
+    # 输出格式
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s: %(message)s "
+    )
+
+    file_handler.setFormatter(formatter)  # 设置文件输出格式
+    console_handler.setFormatter(formatter)  # 设施控制台输出格式
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
 
 
 # 读入文件
@@ -100,66 +255,51 @@ features = np.load('../dataset/processed/comp_feature/entropy_c_features.npy').t
 
 # 生成dgl图
 api_list_array, api_cm_array = create_api_cm(api_df, mashup_df)
-graphs = create_graphs(features, api_df, api_list_array, api_cm_array)
+graphs, max_feature_length = create_graphs(features, api_df, api_list_array, api_cm_array)
 
-# # 准备数据
-# combined_embeddings, train_pairs, labels = prepare_data(graphs)
-# dataset = PairDataset(combined_embeddings, train_pairs, labels)
-# test_size = int(len(dataset) * test_split)
-# train_size = len(dataset) - test_size
-# train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-# train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+# 准备数据
+train_data, test_data, strategies = prepare_data(api_cm_array, api_list_array, api_df, features)
+train_dataset = MyDataset(train_data)
+test_dataset = MyDataset(test_data)
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
 # 初始化模型
 node_nums = []
 for g in graphs:
     node_nums.append(g.number_of_nodes())
-gnn_models = [GraphSAGE(in_feats=embedding_dim, h_feats=embedding_dim).to(device) for _ in range(len(graphs))]
-combiner = Combiner().to(device)
-predictor = Predictor(embedding_dim, h_dim, len(graphs)).to(device)
-mvcg = MVCG(gnn_models, combiner, predictor, embedding_dim, node_nums).to(device)
+mvcg = MVCG(dropout_rate, embedding_dim, node_nums, strategies, device, combine_layer_num).to(device)
+# print(mvcg)
+
+# 实例化
+optimizer = torch.optim.Adam(mvcg.parameters(), lr=learning_rate)
+scheduler = ReduceLROnPlateau(optimizer, mode=s_mode, factor=s_factor, patience=s_patience, verbose=s_verbose)
+early_stopping = EarlyStopping(es_patience, es_delta, es_verbose, path='../model/checkpoint.pt')
+loss_fn = nn.BCEWithLogitsLoss().to(device)  # 用于二分类的交叉熵损失
 
 # 训练&测试模型
-optimizer = torch.optim.Adam(mvcg.parameters(), lr=0.001)
-scheduler = ReduceLROnPlateau(optimizer, 'min', verbose=True, factor=0.5, patience=15)
-early_stopping = EarlyStopping(patience, delta, ifverbose, path='../model/checkpoint.pt')
-loss_fn = nn.BCEWithLogitsLoss().to(device)  # 用于二分类的交叉熵损失
+create_logger('../log')
+print(f'Running on {device}.')
 train_epochs_loss = []
 for epoch in range(num_epochs):
     # 训练
-    print(f'Training begins. Running on {device}.')
-    mvcg.train()
-    train_epoch_loss = []
-    for query, candidate, label in train_loader:
-        # cuda上运行
-        query = query.to(device)
-        candidate = candidate.to(device)
-        label = label.to(device)
-        # 计算complementary score
-        complementary_score = mvcg(graphs, query, candidate)
-        # 计算损失
-        loss = loss_fn(complementary_score, label)
-        # 反向传播和优化
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        # 记录loss
-        train_epoch_loss.append(loss.item())
-    train_epochs_loss.append(np.average(train_epoch_loss))
-    print(f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_epochs_loss[epoch]}')
+    epoch_loss = train(train_dataloader, mvcg, optimizer, loss_fn, max_feature_length, graphs)
+    avg_epoch_loss = np.average(epoch_loss)
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {avg_epoch_loss}')
 
-    # 测试
-    print(f'Testing begins. Running on {device}.')
-    mvcg.eval()
-    with torch.no_grad():
-        pass
+    if((epoch + 1) % 10 == 0):
+        # 测试
+        print(f"----------------------------------------------------------\nEpoch {epoch + 1}")
+        avg_recall, avg_mrr, avg_ndcg = test(test_dataloader, mvcg, graphs, max_feature_length, topk)
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Recall: {avg_recall}')
+        print(f'Epoch [{epoch + 1}/{num_epochs}], MRR: {avg_mrr}')
+        print(f'Epoch [{epoch + 1}/{num_epochs}], NDCG: {avg_ndcg}')
 
     # 调整学习率
-    scheduler.step(train_epoch_loss[-1])
+    scheduler.step(avg_epoch_loss)
 
     # 早停
-    early_stopping(train_epochs_loss[-1])
+    early_stopping(avg_epoch_loss)
     if early_stopping.early_stop:
         print("Early stopping")
         break
--- model/model2.py
+++ model/model2.py
@@ -2,12 +2,15 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from dgl.nn.pytorch import SAGEConv
+from torch.utils.data import Dataset
+import sys
+
 
 class GraphSAGE(nn.Module):
-    def __init__(self, in_feats, h_feats):
+    def __init__(self, embedding_dim):
         super(GraphSAGE, self).__init__()
-        self.conv1 = SAGEConv(in_feats, h_feats, aggregator_type='mean')
-        self.conv2 = SAGEConv(h_feats, h_feats, aggregator_type='mean')
+        self.conv1 = SAGEConv(embedding_dim, embedding_dim, aggregator_type='mean')
+        self.conv2 = SAGEConv(embedding_dim, embedding_dim, aggregator_type='mean')
 
     def forward(self, g, inputs):
         h = self.conv1(g, inputs, edge_weight=g.edata['weight'])  # 在计算中将权重转换为 float
@@ -16,23 +19,84 @@ class GraphSAGE(nn.Module):
         return h
 
 
-class Combiner(nn.Module):
-    def __init__(self):
-        super(Combiner, self).__init__()
+class SelfAttention(nn.Module):
+    def __init__(self, embedding_dim):
+        super(SelfAttention, self).__init__()
+        self.emb_dim = embedding_dim
+        self.Q_linear = nn.Linear(self.emb_dim, self.emb_dim, bias=False)
+        self.K_linear = nn.Linear(self.emb_dim, self.emb_dim, bias=False)
+        self.V_linear = nn.Linear(self.emb_dim, self.emb_dim, bias=False)
 
     def forward(self, embeddings):
-        pass
+        query = self.Q_linear(embeddings)
+        key = self.K_linear(embeddings)
+        value = self.V_linear(embeddings)
+        attention_scores = (torch.matmul(query, key.transpose(-2, -1)) /
+                            torch.sqrt(torch.tensor(self.emb_dim, dtype=torch.float64)))  # emb_dim比较大的时候除以sqrt(emb_dim)比较好，例如emb_dim=512时
+        attention_weights = torch.softmax(attention_scores, dim=-1)
+        attention_result = torch.matmul(attention_weights, value)
+        return attention_result
+
+
+class CombinerLayer(nn.Module):
+    def __init__(self, embedding_dim, dropout_rate):
+        super(CombinerLayer, self).__init__()
+        self.self_attention = SelfAttention(embedding_dim)
+        self.attention_dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, embeddings):
+        # layer normalization
+
+        # self-attention
+        sa_embeddings = self.self_attention(embeddings)
+        # dropout
+        # sa_embeddings = self.attention_dropout(sa_embeddings)
+        # residual
+        # sa_embeddings = sa_embeddings + embeddings
+
+        # layer normalization
+
+        # feed forward
+
+        # dropout
+
+        # residual
+
+
+        return sa_embeddings
+
+
+class Combiner(nn.Module):
+    def __init__(self, embedding_dim, dropout_rate, combine_layer_num):
+        super(Combiner, self).__init__()
+        layers1 = [CombinerLayer(embedding_dim, dropout_rate) for _ in range(combine_layer_num)]
+        layers2 = [CombinerLayer(embedding_dim, dropout_rate) for _ in range(combine_layer_num)]
+        self.layers1 = nn.ModuleList(layers1)
+        self.layers2 = nn.ModuleList(layers2)
+
+    def forward(self, embeddings):
+        output0 = embeddings
+
+        for layer in self.layers1:
+            output0 = layer(output0)
+        output1 = output0.sum(dim=-2)
+
+        for layer in self.layers2:
+            output1 = layer(output1)
+        output2 = output1.sum(dim=-2)
+
+        return output2
 
 
 class Predictor(nn.Module):
-    def __init__(self, embedding_dim, h_dim, graph_num):
+    def __init__(self, embedding_dim):
         super(Predictor, self).__init__()
         self.linear = nn.Sequential(
-            nn.Linear(embedding_dim * graph_num, embedding_dim),
+            nn.Linear(embedding_dim * 2, embedding_dim),
             nn.ReLU(),
-            nn.Linear(embedding_dim, h_dim),
+            nn.Linear(embedding_dim, embedding_dim),
             nn.ReLU(),
-            nn.Linear(h_dim, 1)
+            nn.Linear(embedding_dim, 1)
         )
 
     def forward(self, combined_query_embedding, combined_cand_embedding):
@@ -42,39 +106,139 @@ class Predictor(nn.Module):
 
 
 class MVCG(nn.Module):
-    def __init__(self, gnn_models, attn_combiner, predictor_model, embedding_dim, node_nums):
+    def __init__(self, dropout_rate, embedding_dim, node_nums, strategies, device, combine_layer_num):
         super(MVCG, self).__init__()
-        self.node_embeddings = []
-        for num in node_nums:
-            embedding = nn.Embedding(num, embedding_dim)
-            self.node_embeddings.append(embedding(torch.arange(num)))
-        self.gnn_models = gnn_models
-        self.attn_combiner = attn_combiner
-        self.predictor_model = predictor_model
+        self.device = device
+        self.strategies = strategies
+        self.node_nums = node_nums
 
-    def forward(self, graphs, query, candidate):
+        self.gnn_model0 = GraphSAGE(embedding_dim)
+        self.node_embedding0 = nn.Embedding(node_nums[0], embedding_dim)
+        self.gnn_model1 = GraphSAGE(embedding_dim)
+        self.node_embedding1 = nn.Embedding(node_nums[1], embedding_dim)
+        self.gnn_model2 = GraphSAGE(embedding_dim)
+        self.node_embedding2 = nn.Embedding(node_nums[2], embedding_dim)
+        self.gnn_model3 = GraphSAGE(embedding_dim)
+        self.node_embedding3 = nn.Embedding(node_nums[3], embedding_dim)
+        self.combiner = Combiner(embedding_dim, dropout_rate, combine_layer_num)
+        self.predictor_model = Predictor(embedding_dim)
+
+    def padding_embeddings(self, batch_item, embeddings, feature_length):
+        batch_item_embeddings = []
+        for item in batch_item:
+            # 初始化item_embeddings
+            graph_num = len(embeddings)
+            item_embeddings = [None] * graph_num
+            # 在 feature 维度进行padding
+            for graph_idx, node_indices in item.items():
+                selected_embeddings = embeddings[graph_idx][node_indices].to(self.device)
+                padding_size_nodes = feature_length - selected_embeddings.size(0)  # 计算节点维度的 padding 大小
+                if padding_size_nodes > 0:
+                    pad_nodes = (0, 0, 0, padding_size_nodes)  # 在节点维度（第一维）进行 padding
+                    padded_embeddings = F.pad(selected_embeddings, pad_nodes)
+                else:
+                    padded_embeddings = selected_embeddings
+                item_embeddings[graph_idx] = padded_embeddings
+            # 在 graph 维度进行padding
+            for i in range(graph_num):
+                if item_embeddings[i] is None:
+                    item_embeddings[i] = torch.zeros(feature_length, 64).to(self.device)  # 如果某个图没有嵌入，填充全零张量
+            item_embeddings = torch.stack(item_embeddings)
+            batch_item_embeddings.append(item_embeddings)
+        batch_item_embeddings = torch.stack(batch_item_embeddings).to(self.device)
+        return batch_item_embeddings
+
+    def forward(self, graphs, batch_query, batch_candidate, max_feature_length):  # query和candidate是组合策略，是字典形式
         # 生成node embeddings
         embeddings = []
-        for i, gnn_model in enumerate(self.gnn_models):
-            embedding = gnn_model(graphs[i], self.node_embeddings[i])
-            embeddings.append(embedding)
+        embeddings.append(self.gnn_model0(graphs[0], self.node_embedding0(torch.arange(self.node_nums[0]).to(self.device))))
+        embeddings.append(self.gnn_model1(graphs[1], self.node_embedding1(torch.arange(self.node_nums[1]).to(self.device))))
+        embeddings.append(self.gnn_model2(graphs[2], self.node_embedding2(torch.arange(self.node_nums[2]).to(self.device))))
+        embeddings.append(self.gnn_model3(graphs[3], self.node_embedding3(torch.arange(self.node_nums[3]).to(self.device))))
 
+        batch_query_strategy = []
+        batch_candidate_strategy = []
+        for query in batch_query:
+            batch_query_strategy.append(self.strategies[query])
+        if batch_candidate.dim() > 0:
+            for candidate in batch_candidate:
+                batch_candidate_strategy.append(self.strategies[candidate])
+        else:
+            batch_candidate_strategy.append(self.strategies[batch_candidate])
         # 组合node embeddings
-        query_embeddings = []
-        for graph_idx, node_indices in query.items():
-            query_embeddings.append(embeddings[graph_idx][node_indices])
-        query_embeddings = torch.cat(query_embeddings, dim=0)
-        combined_query_embedding = self.attn_combiners(query_embeddings)
-        candidate_embeddings = []
-        for graph_idx, node_indices in candidate.items():
-            candidate_embeddings.append(embeddings[graph_idx][node_indices])
-        candidate_embeddings = torch.cat(candidate_embeddings, dim=0)
-        combined_cand_embedding = self.attn_combiners(candidate_embeddings)
+        batch_query_embeddings = self.padding_embeddings(batch_query_strategy, embeddings, max_feature_length)
+        batch_combined_query_embedding = self.combiner(batch_query_embeddings)
+
+        batch_cand_embeddings = self.padding_embeddings(batch_candidate_strategy, embeddings, max_feature_length)
+        batch_combined_cand_embedding = self.combiner(batch_cand_embeddings)
 
         # 计算互补得分
-        complementary_score = self.predictor_model(combined_query_embedding, combined_cand_embedding)
+        batch_complementary_score = self.predictor_model(batch_combined_query_embedding, batch_combined_cand_embedding)
 
-        return complementary_score
+        return batch_complementary_score
+
+
+class MyDataset(Dataset):
+    def __init__(self, data):
+        super(MyDataset, self).__init__()
+        self.data = data
+        self.query_data = [item[0] for item in data]
+        self.cand_data = [item[1] for item in data]
+        self.label_data = [item[2] for item in data]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        query_item = self.query_data[idx]
+        cand_item = self.cand_data[idx]
+        label = self.label_data[idx]
+        return query_item, cand_item, label
+
+
+class EarlyStopping:
+    def __init__(self, patience=5, delta=0, verbose=False, path='checkpoint.pt'):
+        self.patience = patience  # 容忍的验证集性能连续下降的次数
+        self.delta = delta  # 控制是否认为性能提升
+        self.verbose = verbose  # 是否输出详细信息
+        self.path = path  # 保存模型参数的路径
+        self.counter = 0  # 计数器，记录验证集性能连续下降的次数
+        self.best_score = None  # 最佳验证集性能
+        self.early_stop = False  # 是否停止训练标志
+
+    def __call__(self, val_loss):
+        if self.best_score is None:
+            self.best_score = val_loss
+            # self.save_checkpoint(model)
+        elif val_loss >= self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = val_loss
+            self.counter = 0
+            # self.save_checkpoint(model)
+
+    def save_checkpoint(self, model):
+        if self.verbose:
+            print(f'Saving model parameters to {self.path}')
+        torch.save(model.state_dict(), self.path)
+
+
+class Logger(object):
+    def __init__(self, filename="output_log.txt"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
 
 
 
